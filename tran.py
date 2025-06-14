c83a540db31b5234a81c27f87b7870774851af7d
niuniu <3483937346@qq.com>
2025-06-14 09:47:14 +0800

    delet

--- tran.py
+++
@@ -1,6 +0,0 @@
-from ultralytics import YOLO
-model = YOLO('/Volumes/Game/ultralytics 8.3.114/runs/detect/eucalyptus_advanced/weights/best.pt')
-# # success = model.export(format="onnx",imgsz=128,half = True,optimize =True,device="cpu") # wrong
-success = model.export(format="onnx",imgsz=640,device="cpu", opset=12,simplify=True)
-# # Export the model to NCNN format
-model.export(format="ncnn",imgsz=640,device="cpu")  # creates '/yolov8n_ncnn_model'
--- val.py
+++
@@ -1,8 +0,0 @@
-from ultralytics import YOLO
-import matplotlib
-matplotlib.use( "TkAgg")
-if __name__ == '__main__':
-    #加载训练好的模型
-    model = YOLO('/Volumes/Game/ultralytics 8.3.114/runs/detect/main_net/weights/best.pt')
-    # 对验证集进行评估
-    metrics = model.val(data = '/Volumes/Game/ultralytics 8.3.114/data.yaml')
